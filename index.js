38472d32f3a0d09543e729946b6f8d8b77506372
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-13 16:52:35 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ element1 = document.querySelector('form#create-task-form');
 element2 = document.querySelector('ul#tasks'); 
 element2.innerHTML = element1
 let inputValue = document.getElementById("new-task-description").value;
+
+inputValue2 = document.createTextNode(inputValue);
+
+element2.appendChild(inputValue2);
