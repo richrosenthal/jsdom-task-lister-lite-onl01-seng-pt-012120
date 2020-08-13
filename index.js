3f771e45b5747aee5643c1dc1abaceda6750c052
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-13 16:47:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,4 @@
+element1 = document.querySelector('form#create-task-form'); 
+element2 = document.querySelector('ul#tasks'); 
+element2.innerHTML = element1
+let inputValue = document.getElementById("new-task-description").value;
