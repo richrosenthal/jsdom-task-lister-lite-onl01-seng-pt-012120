e0df719077e3ffc7b0338b2f6221c2a78617857c
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-16 22:29:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 
 document.querySelector("#create-task-form").addEventListener("click", function(event) {
   let inputValue = document.getElementById("new-task-description").value;
-
-inputValue2 = document.createTextNode(inputValue);
-         document.getElementById("list").innerHTML += inputValue2;
-         event.preventDefault();
-}, false);
-
-document.querySelector("#create-task-form").addEventListener("click", function(event) {
-  let inputValue = document.getElementById("new-task-description").value;
-         document.getElementById("list").innerHTML += (inputValue + "</br>");
+         document.getElementById("list").innerHTML += ("</br>" + inputValue + "</br>");
          event.preventDefault();
 }, false);
 
 
-element1 = document.querySelector('form#create-task-form'); 
-element2 = document.querySelector('ul#tasks'); 
-element2.innerHTML = element1
-let inputValue = document.getElementById("new-task-description").value;
-
-inputValue2 = document.createTextNode(inputValue);
-
-element2.appendChild(inputValue2);
