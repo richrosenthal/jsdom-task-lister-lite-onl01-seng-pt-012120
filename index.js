6db5562a291235bc0106ecb4a3e7068ffd7ce42d
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-16 22:10:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+
+document.querySelector("#id-checkbox").addEventListener("click", function(event) {
+         document.getElementById("output-box").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+         event.preventDefault();
+}, false);
+
+
+
+
 element1 = document.querySelector('form#create-task-form'); 
 element2 = document.querySelector('ul#tasks'); 
 element2.innerHTML = element1
