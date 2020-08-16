73837abd5c195c727c89de6443d4816c073c604f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-08-16 22:31:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-element1 = document.querySelector('form#create-task-form'); 
-element2 = document.querySelector('ul#tasks'); 
-element2.innerHTML = element1
-let inputValue = document.getElementById("new-task-description").value;
 
-inputValue2 = document.createTextNode(inputValue);
+document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+  let inputValue = document.getElementById("new-task-description").value;
+         document.getElementById("list").innerHTML += ("</br>" + inputValue + "</br>");
+         event.preventDefault();
+}, false);
 
-element2.appendChild(inputValue2);
+
